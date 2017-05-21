379cb8af4a7ee3818ca2aeb5fb7c1b8ffd7a8c0e
Kenneth LaMantia <kennethlamantia@gmail.com>
2017-05-21 13:23:47 -0400

    style: add comments to sql file

--- tournament.sql
+++ tournament.sql
@@ -11,16 +11,16 @@ CREATE DATABASE tournament;
 DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS players;
 
-CREATE TABLE players (
+CREATE TABLE players ( -- stores info on players in tournament
   id  serial PRIMARY KEY,
-  name text NOT NULL,
-  record integer DEFAULT 0
+  name text NOT NULL, -- name of the player
+  record integer DEFAULT 0 -- number of wins
 );
 
-CREATE TABLE matches (
-  id serial PRIMARY KEY,
-  match_id integer NOT NULL,
-  player_id integer REFERENCES players (id),
-  result integer CHECK (result = 0 OR result = 1),
-  round_num integer NOT NULL
+CREATE TABLE matches ( -- stores the per match info for the tournament
+  id serial PRIMARY KEY, -- unique to each player per match
+  match_id integer NOT NULL, -- to identify a match
+  player_id integer REFERENCES players (id), -- player id from players table
+  result integer CHECK (result = 0 OR result = 1), -- 0 for loss 1 for win
+  round_num integer NOT NULL -- number of round starting at 1
 );
